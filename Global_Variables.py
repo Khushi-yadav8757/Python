d1f89b743e43ee1a4364a0224d0953db101ebc97
Khushi Yadav <kky957242@gmail.com>
2025-12-14 01:25:53 +0530

    Create Global_Variables.py

---
+++ Global_Variables.py
@@ -0,0 +1,4 @@
+Global Variables
+Variables that are created outside of a function (as in all of the examples in the previous pages) are known as global variables.
+
+Global variables can be used by everyone, both inside of functions and outside.

1477eae6ff75e5cb8bbb46fb7ea24ac94d971f47
Khushi Yadav <kky957242@gmail.com>
2026-01-03 21:54:00 +0530

    Create Validpara.py

---
+++ Validpara.py
@@ -0,0 +1,14 @@
+8. Valid Parentheses
+s = "()[]{}"
+stack = []
+mp = {')':'(', ']':'[', '}':'{'}
+
+for c in s:
+    if c in mp:
+        if not stack or stack.pop() != mp[c]:
+            print(False)
+            break
+    else:
+        stack.append(c)
+else:
+    print(True)

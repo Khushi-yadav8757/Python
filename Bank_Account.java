3662c7bae5363d7a5c9b6a253d8448b0fdcf257d
Khushi Yadav <kky957242@gmail.com>
2026-01-08 14:57:01 +0530

    Update Bank_Account.java

--- Bank_Account.java
+++ Bank_Account.java
@@ -2,19 +2,15 @@ class BankAccount:
     def __init__(self, name, balance=0):
         self.name = name
         self.balance = balance
-
     def deposit(self, amount):
         self.balance += amount
-
     def withdraw(self, amount):
         if amount > self.balance:
             print("Insufficient funds")
         else:
             self.balance -= amount
-
     def get_balance(self):
         return self.balance
-
 # Test
 acc = BankAccount("Khushi", 1000)
 acc.deposit(500)

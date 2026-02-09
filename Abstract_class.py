395617d2f4e54983322665f1aef74cb694e36a7d
Khushi Yadav <kky957242@gmail.com>
2026-02-09 23:32:39 +0530

    Update Abstract_class.py

--- Abstract_class.py
+++ Abstract_class.py
@@ -5,9 +5,7 @@ from abc import ABC, abstractmethod
 class Shape(ABC):
     @abstractmethod
     def area(self):
-        pass
-
-class Circle(Shape):
+        passclass Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 

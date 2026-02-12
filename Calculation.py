51f0949fe168e98cd42ee2cecc68f2fdfdd73b5c
Khushi Yadav <kky957242@gmail.com>
2026-02-12 22:59:53 +0530

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -1,5 +1,4 @@
 # Write a function calculation() such that it accepts two variables and calculation the addition & subtraction & division.
-
 def calculation(a,b):
     print("A :",a)
     print("B :",b)

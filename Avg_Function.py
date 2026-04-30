a5c9977637da61c1a6c77117de61b353f38c7ddd
Khushi Yadav <kky957242@gmail.com>
2026-04-30 15:21:55 +0530

    Update Avg_Function.py

--- Avg_Function.py
+++ Avg_Function.py
@@ -1,9 +1,10 @@
-#wap to find the aveargeusing function
-#a=23
-#b=37
-#c=75
-#avg=(a+b+c)/3
-#print(avg)
+wap to find the aveargeusing function
+a=23
+b=37
+c=75
+avg=(a+b+c)/3
+print(avg)
+
 def avg():
     a=int(input("enter your number: "))
     b=int(input("enter your number: "))

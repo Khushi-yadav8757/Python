b43c18169cbe2167600a54854b505da6f96adf0b
Khushi Yadav <kky957242@gmail.com>
2026-05-30 21:25:42 +0530

    Update Element.py

--- Element.py
+++ Element.py
@@ -1,5 +1,5 @@
 #Find common elements between two lists
-def common_elements(list1, list2):
+def common_elements(list1, list2): 
     return list(set(list1) & set(list2))
     
 print(common_elements([1, 2, 3, 4], [3, 4, 5, 6]))  
